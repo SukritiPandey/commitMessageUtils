f7f55235abbf1e411f93a07b1fb91006abc34af3
SukritiPandey <49089629+SukritiPandey@users.noreply.github.com>
2024-02-25 12:28:22 +0000

    Update parseCommitMessage.groovy

--- vars/parseCommitMessage.groovy
+++ vars/parseCommitMessage.groovy
@@ -1,14 +1,15 @@
 def call(String commitMessage) {
-    def commitInfo = [:]
+    // def commitInfo = [:]
 
-    // Split the commit message into lines
-    def lines = commitMessage.trim().split('\n')
+    // // Split the commit message into lines
+    // def lines = commitMessage.trim().split('\n')
 
-    // The first line is the commit title
-    commitInfo['title'] = lines[0].trim()
+    // // The first line is the commit title
+    // commitInfo['title'] = lines[0].trim()
 
-    // The rest of the lines are the commit body
-    commitInfo['body'] = lines[1..-1].collect { it.trim() }.join('\n')
+    // // The rest of the lines are the commit body
+    // commitInfo['body'] = lines[1..-1].collect { it.trim() }.join('\n')
+    log.info("Durga Prasad Rath")
 
     return this
 }
